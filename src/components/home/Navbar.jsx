734953193d2d9462fb52e6b882138fa7e7cda39e
iamRahul21 <rahuldeka072@gmail.com>
2025-07-02 22:07:47 +0530

    navbar 1.0

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import '../../styles/navbar.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-const Navbar = () => {
+export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="kaizen-navbar">
+      <section className="kaizen-container">
+        <div className="kaizen-logo">
+          <i className="fa-solid fa-shop"></i>
+        </div>
 
-export default Navbar
+        <ul className="kaizen-menu">
+          <li className="kaizen-menu-list">
+            <a href="" className="kaizen-menu-link">Home</a>
+          </li>
+          <li className="kaizen-menu-list">
+            <a href="/about" className="kaizen-menu-link">About</a>
+          </li>
+          <li className="kaizen-menu-list">
+            <a href="/career" className="kaizen-menu-link">Career</a>
+          </li>
+          <li className="kaizen-menu-list">
+            <a href="/blog" className="kaizen-menu-link">Blog</a>
+          </li>
+          <li className="kaizen-menu-list">
+            <a href="/contact-us" className="kaizen-menu-link">Contact Us</a>
+          </li>
+        </ul>
+      </section>
+    </nav>
+  );
+}
